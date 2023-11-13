d393dc9aba1a1eb69b3ce470cab99212c7c255b3
DaviGel <geloso.davide@gmail.com>
2023-11-13 15:01:27 +0100

    feat: esercizio completato

---
+++ js/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const lista = [
+  'Pane',
+  'Farina',
+  'Latte',
+  'Uova',
+  'Cereali',
+  'Biscotti',
+  'Zucchero',
+  'Marmellata',
+  'Acqua',
+];
+
+let index = 0;
+
+while (index < lista.length) {
+  console.log(lista[index]);
+  index++;
+}
